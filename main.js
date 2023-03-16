f85fa2f54d04fc66929e729a212c19f6763646d6
William Sullivan <sullivanwp95@gmail.com>
2023-03-16 19:40:10 -0400

    Display computerSelection in a p tag

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 function getComputerChoice () {
+    let computer = document.querySelector('.computer')
     let computerSelection = Math.floor(Math.random() * 3) + 1
     console.log(computerSelection)
+
+    if (computerSelection === 1) {
+        computer.textContent = "Rock!"
+    } else if (computerSelection === 2) {
+        computer.textContent = "Paper!"
+    } else if (computerSelection === 3) {
+        computer.textContent = "Scissors!"
+    } else {
+        computer.textContent = "ERROR"
+    }
 }
 
 getComputerChoice()
