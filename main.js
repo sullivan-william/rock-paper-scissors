3e76e9bcc7e3155631e8383c887f408f9b62303d
William Sullivan <sullivanwp95@gmail.com>
2023-03-17 17:29:25 -0400

    Adding if statement to make it play until best out of 5

--- main.js
+++ main.js
@@ -37,19 +37,31 @@ function playRound() {
     let playerSelection = getPlayerChoice()
     let computerSelection = getComputerChoice()
 
-    // console.log(`P1: ${playerSelection}, CPU: ${computerSelection}`)
-
     if (playerSelection === 'Rock' && computerSelection === 1 || playerSelection === 'Paper' && computerSelection === 2 || playerSelection === 'Scissors' && computerSelection === 3) {
         document.querySelector('.results').textContent = "IT'S A TIE!"
     } else if (playerSelection === 'Rock' && computerSelection === 2 || playerSelection === 'Paper' && computerSelection === 3 || playerSelection === 'Scissors' && computerSelection === 1) {
         document.querySelector('.results').textContent = "YOU LOSE!"
         cpu += 1
-        console.log(`CPU: ${cpu}`)
+        document.querySelector('.score').textContent = `CPU: ${cpu} vs P1: ${p1}`
     } else if (playerSelection === 'Rock' && computerSelection === 3 || playerSelection === 'Paper' && computerSelection === 1 || playerSelection === 'Scissors' && computerSelection === 2) {
         document.querySelector('.results').textContent = "YOU WIN!"
         p1 += 1
-        console.log(`P1: ${p1}`)
+        document.querySelector('.score').textContent = `CPU: ${cpu} vs P1: ${p1}`
     } else {
         document.querySelector('.results').textContent = ''
     }
+
+    // Best of 5
+    if (cpu === 3) {
+        console.log("LOSE")
+        cpu = 0
+        p1 = 0
+        document.querySelector('.score').textContent = 'CPU wins! Try again?'
+    } else if (p1 === 3) {
+        console.log("WIN")
+        cpu = 0
+        p1 = 0
+        document.querySelector('.score').textContent = 'You win! Go again?'
+    }
+
 }
